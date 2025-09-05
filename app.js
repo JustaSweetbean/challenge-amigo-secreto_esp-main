ab80b8a04aa7a3be903a93a63396a8e021586011
Sweetbean <nayhaisback@gmail.com>
2025-09-05 00:01:11 -0600

    función agregar amigo creada

---
+++ app.js
@@ -0,0 +1,22 @@
+// Define las variables necesarias
+let listaAmigos = [];
+let nombreAmigo = 0;
+
+// Desarrollo una función para agregar amigos a la lista
+
+function agregarAmigo () {
+    nombreAmigo = document.getElementById('amigo').value;
+    console.log(nombreAmigo);
+    
+    // Corroborar que se haya agregado un nombre
+    if (nombreAmigo == '') {
+        alert('inserte un nombre válido');
+    } else {
+        listaAmigos.push(nombreAmigo);
+        limpiarCaja ();
+    }
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
